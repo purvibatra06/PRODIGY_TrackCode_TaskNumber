4358b00a4e04756efbad9d26032fadd2c277567f
purvibatra06 <purvibatra484@gmail.com>
2025-09-26 12:19:43 +0530

    Create uploadLogoMiddleware.js

---
+++ Job-portal/weblayer/middlewares/uploadLogoMiddleware.js
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../../public/images/uploads/company-logo'));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, `logo-${uniqueSuffix}${ext}`);
+  }
+});
+
+const upload = multer({ storage: storage });
+const uploadLogo = upload.single('logo');
+export default uploadLogo;
